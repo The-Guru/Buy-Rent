dd07352a0cde392fcd40468d47cfc9651a5cccef
Óscar García <ogbaro@gmail.com>
2020-05-07 11:42:23 +0200

    Initial Commit

---
+++ Buy&Rent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Buy&Rent
+//
+//  Created by iMac Óscar on 07/05/2020.
+//  Copyright © 2020 iMac Óscar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
